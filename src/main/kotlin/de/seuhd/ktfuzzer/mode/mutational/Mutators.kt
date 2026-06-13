09b27ac68b4cec98a4572aa5575fbd37f69e19ed
angelinavalijonova <angelina040500@gmail.com>
2026-06-13 11:19:41 +0200

    Implement mutators

--- src/main/kotlin/de/seuhd/ktfuzzer/mode/mutational/Mutators.kt
+++ src/main/kotlin/de/seuhd/ktfuzzer/mode/mutational/Mutators.kt
@@ -2,7 +2,6 @@ package de.seuhd.ktfuzzer.mode.mutational
 
 import kotlin.random.Random
 
-
 /**
  * Character-level mutators from The Fuzzing Book's MutationFuzzer
  * (<https://www.fuzzingbook.org/html/MutationFuzzer.html>), plus [repeatRandomCharacter]. Each
@@ -11,51 +10,42 @@ import kotlin.random.Random
 internal object Mutators {
     /** Deletes one randomly chosen character. */
     fun deleteRandomCharacter(input: String, random: Random): String {
-       // TODO("Exercise 1: delete one randomly chosen character"
-            val length = input.length
-            if (length == 0 ){
-                return "";
-            }
-            val index = random.nextInt(input.length)
-            return input.removeRange(index, index + 1)
+        val length = input.length
+        if (length == 0) {
+            return ""
+        }
 
+        val index = random.nextInt(input.length)
+        return input.removeRange(index, index + 1)
     }
-
 
     /** Inserts one character drawn uniformly from [alphabet] at a random position. */
     fun insertRandomCharacter(input: String, alphabet: List<Char>, random: Random): String {
-        //TODO("Exercise 1: insert one character from the alphabet at a random position")
-
-        var result = input.toMutableList()
+        val result = input.toMutableList()
         val randomCharacter = alphabet[random.nextInt(alphabet.size)]
-        val index = random.nextInt(input.length+1)
+        val index = random.nextInt(input.length + 1)
+
         result.add(index, randomCharacter)
         return result.joinToString("")
-
     }
 
-
     /** Flips one randomly chosen low bit of one randomly chosen character. */
-   fun flipRandomCharacter(input: String, random: Random): String {
-        //TODO("Exercise 1: flip one random low bit of one randomly chosen character")
+    fun flipRandomCharacter(input: String, random: Random): String {
         if (input.isEmpty()) return ""
 
         val result = input.toMutableList()
         val index = random.nextInt(input.length)
+        val bit = 1 shl random.nextInt(8)
 
-        val bit = 1 shl random.nextInt(8) // pick a single low bit
         result[index] = (result[index].code xor bit).toChar()
-
         return result.joinToString("")
     }
 
-
     /** Repeats one randomly chosen character a random number of times in place. */
     fun repeatRandomCharacter(input: String, random: Random): String {
-        //TODO("Exercise 1: repeat one randomly chosen character a random number of times")
         if (input.isEmpty()) return ""
 
-        var result = input.toMutableList()
+        val result = input.toMutableList()
         val index = random.nextInt(input.length)
         val counter = random.nextInt(1, 1000)
         val repeatedCharacter = result[index]
@@ -63,6 +53,7 @@ internal object Mutators {
         repeat(counter) {
             result.add(index, repeatedCharacter)
         }
+
         return result.joinToString("")
     }
 }
